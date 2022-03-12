5963a8b562e60b63a2f3e55ca58f7783883fb1cb
Ro Soliveres <roumaine.soliveres@gmail.com>
2022-03-12 18:32:52 +0800

    Started Module 3

---
+++ GAMENET Module 3/GAMENET Module 3/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    [Header("Login UI")]
+    public GameObject LoginUIPanel;
+    public InputField PlayerNameInput;
+
+    [Header("Connecting Info Panel")]
+    public GameObject ConnectingInfoUIPanel;
+
+    [Header("Creating Room Info Panel")]
+    public GameObject CreatingRoomInfoUIPanel;
+
+    [Header("GameOptions  Panel")]
+    public GameObject GameOptionsUIPanel;
+
+    [Header("Create Room Panel")]
+    public GameObject CreateRoomUIPanel;
+    public InputField RoomNameInputField;
+    public string GameMode;
+
+    [Header("Inside Room Panel")]
+    public GameObject InsideRoomUIPanel;
+   
+    [Header("Join Random Room Panel")]
+    public GameObject JoinRandomRoomUIPanel;
+
+    #region Unity Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivatePanel(LoginUIPanel.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region UI Callback Methods
+    public void OnLoginButtonClicked()
+    {
+        string playerName = PlayerNameInput.text;
+
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            ActivatePanel(ConnectingInfoUIPanel.name);
+
+            if (!PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.LocalPlayer.NickName = playerName;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        else
+        {
+            Debug.Log("PlayerName is invalid!");
+        }
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        ActivatePanel(GameOptionsUIPanel.name);
+    }
+    
+    public void OnCreateRoomButtonClicked()
+    {
+        ActivatePanel(CreatingRoomInfoUIPanel.name);
+        CreateRoom();
+    }
+
+    public void OnJoinRandomRoomClicked(string gameMode)
+    {
+        GameMode = gameMode;
+
+        ExitGames.Client.Photon.Hashtable expectedCustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "gm", gameMode } };
+        PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 0);  // 0 means not setting a max number of players
+    }
+
+    public void OnBackButtonClicked()
+    {
+        ActivatePanel(GameOptionsUIPanel.name);
+    }
+    #endregion
+
+    #region Photon Callbacks
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName+ " is connected to Photon");
+        ActivatePanel(GameOptionsUIPanel.name);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom + " has been created!");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " has joined " + PhotonNetwork.CurrentRoom.Name);
+        Debug.Log("Player count = " + PhotonNetwork.CurrentRoom.PlayerCount);
+
+        ActivatePanel(InsideRoomUIPanel.name);
+        object gameModeName;
+        // check to see what is the game mode of the room the player just joined in
+        if(PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("gm", out gameModeName))
+        {
+            Debug.Log(gameModeName.ToString());
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log(message);
+
+        CreateRoom();
+    }
+
+    #endregion
+
+    #region Public Methods
+    public void ActivatePanel(string panelNameToBeActivated)
+    {
+        LoginUIPanel.SetActive(LoginUIPanel.name.Equals(panelNameToBeActivated));
+        ConnectingInfoUIPanel.SetActive(ConnectingInfoUIPanel.name.Equals(panelNameToBeActivated));
+        CreatingRoomInfoUIPanel.SetActive(CreatingRoomInfoUIPanel.name.Equals(panelNameToBeActivated));
+        CreateRoomUIPanel.SetActive(CreateRoomUIPanel.name.Equals(panelNameToBeActivated));
+        GameOptionsUIPanel.SetActive(GameOptionsUIPanel.name.Equals(panelNameToBeActivated));
+        JoinRandomRoomUIPanel.SetActive(JoinRandomRoomUIPanel.name.Equals(panelNameToBeActivated));
+        InsideRoomUIPanel.SetActive(InsideRoomUIPanel.name.Equals(panelNameToBeActivated));
+    }
+
+    public void SetGameMode(string gameMode)
+    {
+        GameMode = gameMode;
+    }
+    #endregion
+
+    #region Private Methods
+
+    private void CreateRoom()
+    {
+        if (GameMode == null) return;
+
+        string roomName = RoomNameInputField.text;
+
+        if (string.IsNullOrEmpty(roomName))
+        {
+            roomName = "Room " + Random.Range(1000, 10000);
+        }
+
+        RoomOptions roomOptions = new RoomOptions();
+        string[] roomPropertiesInLobby = { "gm" }; // gm = game mode
+
+        //Making a "hashtable" - similar to a dictionary (has a key and corresponding val)
+        /*
+         * Game modes: 
+         * rc = racing
+         * dr = death race
+         */
+        ExitGames.Client.Photon.Hashtable customRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "gm", GameMode } };
+
+        roomOptions.CustomRoomPropertiesForLobby = roomPropertiesInLobby;
+        roomOptions.CustomRoomProperties = customRoomProperties;
+        PhotonNetwork.CreateRoom(roomName, roomOptions);
+    }
+    #endregion
+}
